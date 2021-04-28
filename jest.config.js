0d8ad8ad8368cb3d864256da80607704dec0b436
“alioshr” <alioshmpb@gmail.com>
2021-04-28 00:07:41 -0300

    chore: add jest to lint staged

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@
  * https://jestjs.io/docs/en/configuration.html
  */
 
-export default {
+module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
