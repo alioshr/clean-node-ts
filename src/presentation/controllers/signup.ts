49b553afb10ce738e760155cfc6c4ffc09f4f750
“alioshr” <alioshmpb@gmail.com>
2021-04-29 21:49:25 -0300

    refactor: make SignUpController implement a protocol

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,7 +1,8 @@
 import { badRequest } from './../helpers/http-helper'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
-export class SignUpController {
+import { Controller } from '../protocols/controller'
+export class SignUpController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
     let response: HttpResponse = {
       statusCode: 200,
