7096e3f577134bd53790922ba44ec44c9b7cae49
“alioshr” <alioshmpb@gmail.com>
2021-05-02 04:08:33 -0300

    test: ensure SignUpController return 200 if proper data is provided

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -209,4 +209,23 @@ describe('Auth Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
   })
+  test('Should return 200 if the proper data is provided', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'valid_name',
+        email: 'valid@email.com',
+        password: 'valid_password',
+        confirmPassword: 'valid_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid@email.com',
+      password: 'valid_password'
+    })
+  })
 })
