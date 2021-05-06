5499077aea7092aa9c0483c62d06331eb80c297d
“alioshr” <alioshmpb@gmail.com>
2021-05-06 14:10:50 -0300

    feat: ensure that BCryptAdapter calls bcrypt on the hash method with the proper params

---
+++ src/infra/cryptography/bcrypt-adapter.ts
@@ -0,0 +1,15 @@
+import { Encrypt } from '../../data/protocols'
+import bcrypt from 'bcrypt'
+
+export class BCryptAdapter implements Encrypt {
+  private readonly salt: number
+
+  constructor (salt: number) {
+    this.salt = salt
+  }
+
+  async hash (password: string): Promise<string> {
+    const hashedPassword = await bcrypt.hash(password, this.salt)
+    return hashedPassword
+  }
+}
