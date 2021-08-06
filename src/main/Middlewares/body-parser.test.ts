0db476e21309ca21f7f4ff57f6d1e12058dee88b
alioshr <alioshmpb@gmail.com>
2021-08-06 17:10:49 -0300

    feat: ensure app parses body as json

---
+++ src/main/Middlewares/body-parser.test.ts
@@ -0,0 +1,13 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Body parser middleware', () => {
+  test('Should parse body as json', async () => {
+    app.post('/test_body_parser', (req, res) => res.send(req.body))
+
+    await request(app)
+      .post('/test_body_parser')
+      .send({ name: 'Aliosh' })
+      .expect({ name: 'Aliosh' })
+  })
+})
