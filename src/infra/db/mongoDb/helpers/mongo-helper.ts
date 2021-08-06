885833cbc55b94a8780862a26c3175ffed0254f9
alioshr <alioshmpb@gmail.com>
2021-08-06 15:32:19 -0300

    feat: mongoHelper gets collection

--- src/infra/db/mongoDb/helpers/mongo-helper.ts
+++ src/infra/db/mongoDb/helpers/mongo-helper.ts
@@ -1,20 +1,27 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
 
 interface IMongoHelper {
   client: MongoClient | null
   connect: (uri: string) => Promise<void>
   disconnect: () => Promise<void>
+  getCollection: (name: string) => Collection
 }
 
 export const MongoHelper: IMongoHelper = {
   client: null,
+
   async connect (uri: string) {
     this.client = await MongoClient.connect(process.env.MONGO_URL as string, {
       useUnifiedTopology: true,
       useNewUrlParser: true
     })
   },
+
   async disconnect () {
     await (this.client as MongoClient).close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
   }
 }
