08e9d216c71eede42df59312183fba42d97bb47b
“alioshr” <alioshmpb@gmail.com>
2021-08-07 16:19:36 -0300

    test: ensure that SignUpController throws newServerError if addAccount throws

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -209,6 +209,7 @@ describe('Auth Controller', () => {
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
   })
   test('Should return 200 if the proper data is provided', async () => {
     const { sut } = makeSut()
