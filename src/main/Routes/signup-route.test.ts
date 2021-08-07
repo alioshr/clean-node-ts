d45a42b6ad5af8e6abf6088cedde9ddf7fe1f04f
“alioshr” <alioshmpb@gmail.com>
2021-08-06 21:58:34 -0300

    refactor: add mongodb helper before integration tests

--- src/main/Routes/signup-route.test.ts
+++ src/main/Routes/signup-route.test.ts
@@ -1,10 +1,21 @@
 import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 
 describe('Sign-up route', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    await MongoHelper.getCollection('accounts').deleteMany({})
+  })
+
   test('Should return an account on success', async () => {
-    await request(app)
-      .post('/api/signup')
-      .expect(200)
+    await request(app).post('/api/signup').expect(200)
   })
 })
