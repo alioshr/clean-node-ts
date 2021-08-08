e40d45e3679373edd6114d0cc7d75801b1c5e2e0
“alioshr” <alioshmpb@gmail.com>
2021-08-08 04:41:11 -0300

    refactor: env added to gitignore

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,5 @@
 import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
-import env from './config/env'
+import env from './config/.env'
 
 MongoHelper.connect(env.mongoUrl)
   .then(async () => {
