fbef601ff052710af35f8105e301889a24e1f839
“alioshr” <alioshmpb@gmail.com>
2021-08-09 20:54:15 -0300

    refactor: added implemented logMongoRepositury to signup route

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,6 +1,7 @@
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BCryptAdapter } from '../../infra/cryptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
 import { SignUpController } from '../../presentation/controllers/signup/signup'
 import { Controller } from '../../presentation/protocols'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
@@ -18,9 +19,7 @@ export const makeSignUpController = (): Controller => {
     emailValidatorAdapter,
     dbAddAccount
   )
-  return new LoggerControllerDecorator(signUpController, {
-    logError: async function (stack) {
-      return await Promise.resolve()
-    }
-  })
+
+  const logMongoRepository = new LogMongoRepository()
+  return new LoggerControllerDecorator(signUpController, logMongoRepository)
 }
