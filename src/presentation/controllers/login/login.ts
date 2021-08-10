f587cccd0fb8d084e6c25a0cf803f6e6f446ee7c
“alioshr” <alioshmpb@gmail.com>
2021-08-10 03:03:12 -0300

    feat: ensure LoginController validates missing params and email validation and returns 200 if all ok

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,23 +1,31 @@
-import { InvalidParamError } from '../../errors'
+import { AuthAccount } from '../../../domain/usecases/auth-account'
+import { InvalidParamError, MissingParamError } from '../../errors'
 import { badRequest, ok, serverError } from '../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+import { EmailValidator } from '../signup/signup-protocols'
 
 export class LoginController implements Controller {
+  constructor (
+    private readonly authAccount: AuthAccount,
+    private readonly emailValidator: EmailValidator
+  ) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const bodyFields = ['email', 'password']
       for (const field of bodyFields) {
         if (!httpRequest.body[field]) {
-          return badRequest(new InvalidParamError(field))
+          return badRequest(new MissingParamError(field))
         }
       }
 
-      const httpResponse = {
-        userId: 'a',
-        token: 'a'
+      const isEmailValid = this.emailValidator.isValid(httpRequest.body.email)
+      if (!isEmailValid) {
+        return badRequest(new InvalidParamError('email'))
       }
+      const authAccountResponse = await this.authAccount.auth(httpRequest.body)
 
-      return ok(httpResponse)
+      return ok(authAccountResponse)
     } catch (error) {
       return serverError(error)
     }
