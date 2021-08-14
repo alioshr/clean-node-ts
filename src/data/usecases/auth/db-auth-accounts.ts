bee4f3841a8034cd71b7356737b3a0de96137328
“alioshr” <alioshmpb@gmail.com>
2021-08-14 03:57:16 -0300

    feat: ensure LoadEmailRepository receives a correct email

---
+++ src/data/usecases/auth/db-auth-accounts.ts
@@ -0,0 +1,12 @@
+import { AuthResponseData } from '../../../domain/models/auth'
+import { AuthAccount, AuthAccountModel } from '../../../domain/usecases/auth-account'
+import { LoadAccountRepository } from '../../protocols/load-account-by-email'
+
+export class DbAuthAccount implements AuthAccount {
+  constructor (private readonly loadAccount: LoadAccountRepository) {}
+
+  async auth (authData: AuthAccountModel): Promise<AuthResponseData | Error> {
+    await this.loadAccount.load(authData.email)
+    return await Promise.resolve({ userId: 'a', token: 'a' })
+  }
+}
