53d09c53bf7e94fbb6fd84e884aa84f912577ed0
“alioshr” <alioshmpb@gmail.com>
2021-08-18 02:14:41 -0300

    feat: ensure AccountMongoRepository returns void on success updateToken

--- src/infra/db/mongoDb/account-repository/account.ts
+++ src/infra/db/mongoDb/account-repository/account.ts
@@ -1,13 +1,18 @@
-import { AddUserRepository } from '../../../../data/protocols'
+import { AddUserRepository, UpdateAccessTokenRepository } from '../../../../data/protocols'
 import { AccountModel } from '../../../../domain/models/account'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class AccountMongoRepository implements AddUserRepository {
+export class AccountMongoRepository implements AddUserRepository, UpdateAccessTokenRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const collection = await MongoHelper.getCollection('accounts')
     const result = await collection.insertOne(accountData)
     const account = result.ops[0]
     return MongoHelper.map(account)
+  }
+
+  async updateToken (data: { token: string, id: string }): Promise<void> {
+    const collection = await MongoHelper.getCollection('accounts')
+    await collection.updateOne({ _id: data.id }, { $set: { token: data.token } })
   }
 }
