259a52490197006e541e93eb3c4b4daf60597e88
“alioshr” <alioshmpb@gmail.com>
2021-08-19 20:08:20 -0300

    refactor: ensure routes are relative

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,14 +1,16 @@
 import { Express, Router } from 'express'
-import { readdirSync } from 'fs'
-import path from 'path'
+// import { readdirSync } from 'fs'
+// import path from 'path'
+import authRoutes from '../Routes/authentication-route'
 
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  readdirSync(path.join(__dirname, '/../Routes')).map(async (file) => {
-    if (!file.includes('.spec.')) {
-      return (await import(`../Routes/${file}`)).default(router)
-    }
-  }
-  )
+  authRoutes(router)
+  // readdirSync(path.join(__dirname, './../Routes')).map(async (file) => {
+  //   if (!file.includes('.spec.')) {
+  //     return (await import(`../Routes/${file}`)).default(router)
+  //   }
+  // }
+  // )
 }
