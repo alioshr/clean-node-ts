f829f5adf642e1675ab74c2ae6c2e166d70b8cb6
“alioshr” <alioshmpb@gmail.com>
2021-08-19 20:23:24 -0300

    fix: adding ts to prod and reverting fs on routes

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,16 +1,14 @@
 import { Express, Router } from 'express'
-// import { readdirSync } from 'fs'
-// import path from 'path'
-import authRoutes from '../Routes/authentication-route'
+import { readdirSync } from 'fs'
+import path from 'path'
 
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  authRoutes(router)
-  // readdirSync(path.join(__dirname, './../Routes')).map(async (file) => {
-  //   if (!file.includes('.spec.')) {
-  //     return (await import(`../Routes/${file}`)).default(router)
-  //   }
-  // }
-  // )
+  readdirSync(path.join(__dirname, './../Routes')).map(async (file) => {
+    if (!file.includes('.spec.')) {
+      return (await import(`../Routes/${file}`)).default(router)
+    }
+  }
+  )
 }
