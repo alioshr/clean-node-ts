5b18a9cb867a38df04842d564bfea9d46d0046be
“alioshr” <alioshmpb@gmail.com>
2021-08-19 15:03:28 -0300

    refactor: grouped login and signup routes in a single file

--- src/main/Routes/login-route.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express'
-import { adaptRoute } from '../adapters/express/express-route-adapter'
-import { makeLoginController } from '../factories/login/login-controller-factory'
-
-export default (router: Router): void => {
-  router.post('/login', adaptRoute(makeLoginController()))
-}
