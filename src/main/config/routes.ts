e34f3511e41e6f0fd7164dc4634cada10ee0ed67
“alioshr” <alioshmpb@gmail.com>
2021-08-21 23:44:12 -0300

    fix: ensure that node does not read map generated by ts

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -6,7 +6,7 @@ export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
   readdirSync(path.join(__dirname, './../Routes')).map(async (file) => {
-    if (!file.includes('.spec.')) {
+    if (!file.includes('.spec.') && !file.includes('.map')) {
       return (await import(`../Routes/${file}`)).default(router)
     }
   }
