4bf34448938b787567cb570b04ea58924969ee65
“alioshr” <alioshmpb@gmail.com>
2021-08-23 20:30:08 -0300

    test: ensure that the code is properly built in heroku with dummy data

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -5,6 +5,11 @@ import path from 'path'
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
+  app.get('/test', (req, res) => {
+    res.json({
+      message: 'test working'
+    })
+  })
   readdirSync(path.join(__dirname, './../Routes')).map(async (file) => {
     if (!file.includes('.spec.') && !file.includes('.map')) {
       return (await import(`../Routes/${file}`)).default(router)
