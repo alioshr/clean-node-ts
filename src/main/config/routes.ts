9fb0ed289469801fdef94778438a07e9edee4b49
“alioshr” <alioshmpb@gmail.com>
2021-08-23 20:35:45 -0300

    test: reverting dummy code on succes build through travis => heroku

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -5,11 +5,6 @@ import path from 'path'
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  app.get('/test', (req, res) => {
-    res.json({
-      message: 'test working'
-    })
-  })
   readdirSync(path.join(__dirname, './../Routes')).map(async (file) => {
     if (!file.includes('.spec.') && !file.includes('.map')) {
       return (await import(`../Routes/${file}`)).default(router)
