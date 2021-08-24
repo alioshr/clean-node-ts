0cdf54c0d344920f56d9a1b92d4fb52f7be276e4
“alioshr” <alioshmpb@gmail.com>
2021-08-24 10:19:37 -0300

    chore: removed install and moved test to script section

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,5 +1,5 @@
 import { Express } from 'express'
-import { bodyParser, contentType, cors } from '../Middlewares'
+import { bodyParser, contentType, cors } from '../middlewares'
 
 export default (app: Express): void => {
   app.use(bodyParser)
