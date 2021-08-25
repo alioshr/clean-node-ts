2c87a09838cfbf4e15be9c0529e14af223c885cb
“alioshr” <alioshmpb@gmail.com>
2021-08-25 17:28:27 -0300

    feat: ensure DbAddAccount returns null if account exists

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -3,16 +3,22 @@ import {
   AddAccount,
   AddAccountModel,
   Hasher,
-  AddUserRepository
+  AddUserRepository,
+  LoadAccountRepository
 } from './db-add-account-protocols'
 
 export class DbAddAccount implements AddAccount {
   constructor (
     private readonly hasher: Hasher,
-    private readonly addUser: AddUserRepository
+    private readonly addUser: AddUserRepository,
+    private readonly loadAccount: LoadAccountRepository
   ) {}
 
-  async add (accountData: AddAccountModel): Promise<AccountModel> {
+  async add (accountData: AddAccountModel): Promise<AccountModel | Error | null> {
+    const account = await this.loadAccount.load(accountData.email)
+    if (account) {
+      return null
+    }
     const hashedPassword = await this.hasher.hash(accountData.password)
     const userData = await this.addUser.add(
       Object.assign({}, accountData, { password: hashedPassword })
